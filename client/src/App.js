db5fac8764068e08f6579eae2cfe16573d0e3ad8
Prateek Jain <prateek.jain@shl.com>
2021-12-03 21:07:14 +0530

    FE CP 1 TIll Topbar

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import Home from "./pages/home/Home";
+import { Person } from "@material-ui/icons";
+import Topbar from "./components/topbar/Topbar";
+function App() {
+    return <Topbar/>;
+}
+
+export default App;
